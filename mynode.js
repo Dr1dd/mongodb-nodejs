ec317bf30dbbe55962127a9b4c4491b0d3709bf5
Laurynas Leišis <lauris.leisis@gmail.com>
2019-11-07 20:14:34 +0200

    first commit

---
+++ mynode.js
@@ -0,0 +1,172 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://192.168.0.106/";
+
+var genresObject = [
+	{_id: 1, genre_title: 'Action'},
+	{_id: 2, genre_title: 'Comedy'},
+];
+var reviewersObject = [
+	{_id: 1, reviewer_name: 'Peter Jackson'},
+	{_id: 3, reviewer_name: 'John Tomston'},
+];
+var moviesObject = [
+	{ _id: 1, movie_title: 'Vienas', movie_year: 1999, movie_genres: [{genre_id: 2},{genre_id:1}], rating: [{reviewer_id: 1, rating_stars: 5}, {reviewer_id: 3, rating_stars:4}]},
+	{ _id: 2, movie_title: 'Du', movie_year: 2003, movie_genres: [{genre_id: 1}], rating: [{reviewer_id: 1, rating_stars: 4}, {reviewer_id: 3, rating_stars:5}]},
+	{ _id: 3, movie_title: 'Trys', movie_year: 2005, movie_genres: [{genre_id: 1},{genre_id:2}], rating: [{reviewer_id: 1, rating_stars: 2}, {reviewer_id: 3, rating_stars:3}]},
+
+]; 
+
+//queryEmbedded();
+//queryAggregation();
+queryMapReduce();
+function createCollections(){
+		MongoClient.connect(url, function(err, db) {
+		  if (err) throw err;
+		  var dbo = db.db("mydb");
+		  dbo.createCollection("movies", function(err, res) {
+		    if (err) throw err;
+		    console.log("Movies collection created!");
+
+		    db.close();
+		  });
+		  dbo.createCollection("reviewers", function(err, res) {
+		    if (err) throw err;
+		    console.log("Rating collection created!");
+		    db.close();
+
+		  });
+		  dbo.createCollection("genres", function(err, res) {
+		    if (err) throw err;
+		    console.log("Genres collection created!");
+		    db.close();
+
+		  });
+		});
+}
+function insertData(){
+	MongoClient.connect(url, function(err, db) {
+		if (err) throw err;
+		var dbo = db.db("mydb");
+	  dbo.collection('reviewers').insertMany(reviewersObject, function(err, res) {
+	  	if (err) throw err;
+	  	console.log("inserter: " + res.insertedCount);
+	    db.close();
+
+	  });
+	    dbo.collection('genres').insertMany(genresObject, function(err, res) {
+	  	if (err) throw err;
+	  	console.log("inserter: " + res.insertedCount);
+	    db.close();
+
+	  });
+	    dbo.collection('movies').insertMany(moviesObject, function(err, res) {
+	  	if (err) throw err;
+	  	console.log("inserter: " + res.insertedCount);
+	    db.close();
+	  });
+	});
+}
+
+function queryEmbedded(){
+
+	MongoClient.connect(url, function(err, db) {
+		var dbo = db.db("mydb");
+		dbo.collection("movies").find({}, { projection: {rating: 1}}).toArray(function(err, result){
+			if (err) throw err;
+
+			console.log(JSON.stringify(result, null, 2));
+			db.close;
+		});
+		});
+}
+function queryAggregation(){
+	MongoClient.connect(url, function(err, db) {
+		var dbo = db.db("mydb");
+		dbo.collection('movies').aggregate([
+			{$unwind: "$rating"},
+			{$project : {_id : 1, movie_title : 1, movie_year : 1, rating: 1 }},
+		    { $lookup:
+		       {
+		         from: 'reviewers',
+		         localField: 'rating.reviewer_id',
+		         foreignField: '_id',
+		         as: 'reviewee',
+		       }
+		     },
+		    ]).toArray(function(err, res) {
+		    if (err) throw err;
+		    console.log(JSON.stringify(res, null, 2));
+		    db.close();
+		});
+		});
+}
+var map = function(){
+	//var ratingArray = new Array();
+	//var ratingArray;
+	/*for(i in this.rating){
+		if(this.rating[i].reviewer_id == ratingArray[i] = this.rating[i].rating_stars;
+	}*/
+	var value = new Array();
+	//for(var i in this.rating){
+		var value = {
+		movie_id: this._id,
+		movie_title: this.movie_title,
+		year: this.movie_year,
+		rating: this.rating,
+		 };
+	//}
+	//if(this.rating.reviewer_id = this.)
+		for(var i in this)
+			for(var j in this.rating)
+		emit({ reviewer_id:this.rating[j].reviewer_id}, value );
+
+}
+var mapReviewers = function(){
+
+		emit({reviewer_id:this._id}, {"name" : this.reviewer_name } );
+}
+var reduce = function(k, v) {
+			var result = {  
+			"reviewer_name" : "",
+			"movie_title": "",
+			"year": "",
+			"rating": [],
+		};
+	
+		//result.reviewer_name
+		
+		 v.forEach(function(v) {
+
+		if(v.movie_title != null ) result.movie_title = v.movie_title;
+		if(v.name != null) result.reviewer_name = v.name;
+		if(v.year != null) result.year = v.year;
+		//if(v.result != null) result.rating = v.result;
+		 
+		 	if(v.rating!=null) result= v.rating;
+		 
+		 });
+		 var resultArray = [];
+		 resultArray.push(result);
+		//console.log(result.year);
+		return {value:resultArray};
+	}
+function queryMapReduce() {
+	MongoClient.connect(url, function(err, db) {
+		var dbo = db.db("mydb");
+		
+		dbo.collection("reviewers").mapReduce(mapReviewers, reduce, { out: { reduce: "joined"} }, 
+					function (err, result) {
+				if(result) {
+					//console.log(result);
+				}
+			db.close();
+		}
+			);
+		dbo.collection('movies').mapReduce(
+			map,
+			reduce,
+			{ out: { reduce: "joined"} },
+			
+			);
+	});
+}
